21807bfe289a293d2abc73c3bb1420caea2266b9
Mmati1996 <mat.duk.at@gmail.com>
2020-10-20 14:38:07 +0200

    Reverser class created

---
+++ ppkwu1/src/main/java/com/example/ppkwu1/Reverser.java
@@ -0,0 +1,13 @@
+package com.example.ppkwu1;
+
+public class Reverser {
+    private final String text;
+
+    public Reverser(String text){
+        this.text = text;
+    }
+
+    public String getText(){
+        return text;
+    }
+}
