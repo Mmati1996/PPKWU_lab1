4230e899ce0a46eef05d2b0cf4f3ee39ac4c20cb
Mmati1996 <57298066+Mmati1996@users.noreply.github.com>
2020-10-20 14:22:27 +0200

    spring init
    sprint init with following settings:
    project: maven
    language: java
    spring boot version: 2.3.4
    packing: jar
    java version: 11
    dependencies: spring web

---
+++ ppkwu1/src/test/java/com/example/ppkwu1/Ppkwu1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ppkwu1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppkwu1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
